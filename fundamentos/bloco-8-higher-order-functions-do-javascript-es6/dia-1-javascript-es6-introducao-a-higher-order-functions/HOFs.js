54dccbfc73f2d8df3e3f7c3ad8cd8466dff18329
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-18 15:39:19 -0300

    Feito aula 8.1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/HOFs.js
@@ -0,0 +1,6 @@
+const wakeUp = () => 'Arcordando!!';
+const breakfeast = () => 'Bora tomar café!!';
+const sleep = () => 'Partiu dormir!!';
+const doingThings = (func) => console.log(func);
+
+doingThings(sleep());
