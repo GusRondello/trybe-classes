7130cab0e5f56c92a799cac3b101e3ee5140570a
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-20 14:56:01 -0300

    Aula 8.2

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/find.js
@@ -0,0 +1,17 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  const divisible = numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+  return divisible
+}
+
+console.log(findDivisibleBy3And5())
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  const letters = names.find((name) => );
+  return letters
+}
+
+console.log(findNameWithFiveLetters());
